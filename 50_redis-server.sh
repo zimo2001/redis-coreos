b843ae46b1c2ff5ec31d1400a349409555915356
Motti Haim <mottih@walla.net.il>
2016-03-24 15:06:09 +0200

    change init script for redis-server

--- 50_redis-server.sh
+++ 50_redis-server.sh
@@ -7,6 +7,10 @@ DAEMON=/usr/bin/redis-server
 DAEMON_ARGS=/etc/redis/redis.conf
 
 
+RUNDIR=/var/run/redis
+PIDFILE=$RUNDIR/redis-server.pid
+
+
 # source /etc/default/redis-server
 if [ -r /etc/default/$NAME ]
 then
@@ -21,4 +25,5 @@ if [ -n "$ULIMIT" ]
 	fi
 
 # run it!
-/sbin/runuser --user ${USER} --group ${GROUP} -s /bin/bash -c "${DAEMON} ${DAEMON_ARGS}"
+
+start-stop-daemon --start --quiet --umask 007 --pidfile $PIDFILE --chuid $USER:$GROUP --exec $DAEMON -- $DAEMON_ARGS
